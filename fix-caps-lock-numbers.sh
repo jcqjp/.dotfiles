2292cf65b20fd43f911e08aa17c1d01e45b3c202
tp222 <tp222@pm.me>
2026-04-11 08:56:44 +0200

    ajoute un script pour écrire des chiffres avec caps lock

---
+++ fix-caps-lock-numbers.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Script pour faire agir Caps Lock comme sous Windows (chiffres en Shift Lock)
+# Executer avec sudo
+
+# Sauvegarde du fichier original (optionnel mais recommandé)
+cp /usr/share/X11/xkb/symbols/fr /usr/share/X11/xkb/symbols/fr.backup
+
+# Insérer l'include à la ligne 5 du fichier fr
+sed -i '5i include "mswindows-capslock"' /usr/share/X11/xkb/symbols/fr
+
+# Créer le nouveau fichier de symboles
+cat > /usr/share/X11/xkb/symbols/mswindows-capslock << 'EOF'
+// Replicate a "feature" of MS Windows on AZERTY keyboards
+// where Caps Lock also acts as a Shift Lock on number keys.
+// Include keys <AE01> to <AE10> in the FOUR_LEVEL_ALPHABETIC key type.
+
+partial alphanumeric_keys
+xkb_symbols "basic" {
+    key <AE01>	{ type= "FOUR_LEVEL_ALPHABETIC", [ ampersand,          1,          bar,   exclamdown ]	};
+    key <AE02>	{ type= "FOUR_LEVEL_ALPHABETIC", [    eacute,          2,           at,    oneeighth ]	};
+    key <AE03>	{ type= "FOUR_LEVEL_ALPHABETIC", [  quotedbl,          3,   numbersign,     sterling ]	};
+    key <AE04>	{ type= "FOUR_LEVEL_ALPHABETIC", [apostrophe,          4,   onequarter,       dollar ]	};
+    key <AE05>	{ type= "FOUR_LEVEL_ALPHABETIC", [ parenleft,          5,      onehalf, threeeighths ]	};
+    key <AE06>	{ type= "FOUR_LEVEL_ALPHABETIC", [   section,          6,  asciicircum,  fiveeighths ]	};
+    key <AE07>	{ type= "FOUR_LEVEL_ALPHABETIC", [    egrave,          7,    braceleft, seveneighths ]	};
+    key <AE08>	{ type= "FOUR_LEVEL_ALPHABETIC", [    exclam,          8,  bracketleft,    trademark ]	};
+    key <AE09>	{ type= "FOUR_LEVEL_ALPHABETIC", [  ccedilla,          9,    braceleft,    plusminus ]	};
+    key <AE10>	{ type= "FOUR_LEVEL_ALPHABETIC", [    agrave,          0,   braceright,       degree ]	};
+};
+EOF
+
+echo "Configuration appliquée. Redémarrez votre session X ou rechargez la configuration clavier."
