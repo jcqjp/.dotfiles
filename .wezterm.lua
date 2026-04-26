fc4ee8891dffe7f440a00235f1f449f01454fe61
Chang Pice <chang@MacBook-Pro-de-Chang.local>
2026-04-26 09:59:45 +0200

    iterm2 colors wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,48 +2,50 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.font_size = 12
-config.font = wezterm.font("MesloLGSDZ Nerd Font")
+config.font = wezterm.font("MesloLGS Nerd Font")
 
 config.hide_tab_bar_if_only_one_tab = true
 --config.window_decorations = "RESIZE"
 
 config.colors = {
-  background = '#0f0f12', -- background-color
-  foreground = '#aaaaaa', -- foreground-color
+  -- Éléments principaux
+  foreground = "#DCDCDC",
+  background = "#14191E",
 
-  -- Palette ANSI (16 couleurs)
+  -- Curseur
+  cursor_bg   = "#FFFFFF",
+  cursor_fg   = "#000000",
+  cursor_border = "#FFFFFF",  -- optionnel, même couleur que cursor_bg
+
+  -- Sélection
+  selection_bg = "#B3D7FF",
+  selection_fg = "#000000",
+
+  -- ANSI (16 couleurs standards, index 0..15)
   ansi = {
-    '#171421', -- noir   (index 0)
-    '#c01c28', -- rouge  (1)
-    '#30d78a', -- vert   (2)
-    '#a2734c', -- jaune  (3)
-    '#1977eb', -- bleu   (4)
-    '#a347ba', -- magenta(5)
-    '#2aa1b3', -- cyan   (6)
-    '#d0cfcc', -- blanc  (7)
+    "#14191E",  -- 0  black
+    "#B43C2A",  -- 1  red
+    "#00C200",  -- 2  green
+    "#C7C400",  -- 3  yellow
+    "#2744C7",  -- 4  blue
+    "#C040BE",  -- 5  magenta
+    "#00C5C7",  -- 6  cyan
+    "#C7C7C7",  -- 7  white
   },
+
   brights = {
-    '#5e5c64', -- noir clair   (8)
-    '#f66151', -- rouge clair   (9)
-    '#33da7a', -- vert clair   (10)
-    '#e9ad0c', -- jaune clair  (11)
-    '#2a7bde', -- bleu clair   (12)
-    '#c061cb', -- magenta clair(13)
-    '#33c7de', -- cyan clair   (14)
-    '#ffffff', -- blanc clair  (15)
+    "#686868",  -- 8  bright black
+    "#DD7975",  -- 9  bright red
+    "#58E790",  -- 10 bright green
+    "#ECE100",  -- 11 bright yellow
+    "#A7ABF2",  -- 12 bright blue
+    "#E17EE1",  -- 13 bright magenta
+    "#60FDFF",  -- 14 bright cyan
+    "#FFFFFF",  -- 15 bright white
   },
-
-  cursor_fg = '#d0cfcc',      -- couleur du caractère sous le curseur (optionnel)
-  cursor_bg = '#d0cfcc',      -- couleur de fond du curseur (le curseur lui-même)
-  cursor_border = '#d0cfcc',
-
-  tab_bar = {
-    active_tab = {
-      bg_color = '#0f0f12',
-      fg_color = '#d0cfcc'
-    }
-  }
 }
+
+config.color_scheme = "Custom Dark"
 
 config.initial_cols = 120
 config.initial_rows = 50
