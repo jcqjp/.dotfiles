4144b4cc4f4b2941f6eee09fe9b6d8dbf88d645e
user1 <user1@MacBook-Pro-de-user1.local>
2026-05-03 10:35:45 +0200

    removed fix cap script and added font

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,10 +2,11 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.font_size = 12
-config.font = wezterm.font("MesloLGS Nerd Font")
+config.font = wezterm.font("MesloLGSDZ Nerd Font")
 
 config.hide_tab_bar_if_only_one_tab = true
---config.window_decorations = "RESIZE"
+config.window_decorations = "RESIZE"
+config.window_close_confirmation = "NeverPrompt"
 
 config.colors = {
   -- Éléments principaux
@@ -13,39 +14,40 @@ config.colors = {
   background = "#14191E",
 
   -- Curseur
-  cursor_bg   = "#FFFFFF",
-  cursor_fg   = "#000000",
-  cursor_border = "#FFFFFF",  -- optionnel, même couleur que cursor_bg
+  cursor_bg = "#FFFFFF",
+  cursor_fg = "#000000",
+  cursor_border = "#FFFFFF",
 
   -- Sélection
   selection_bg = "#B3D7FF",
   selection_fg = "#000000",
 
-  -- ANSI (16 couleurs standards, index 0..15)
+  -- ANSI (16 couleurs standards)
   ansi = {
-    "#14191E",  -- 0  black
-    "#B43C2A",  -- 1  red
-    "#00C200",  -- 2  green
-    "#C7C400",  -- 3  yellow
-    "#2744C7",  -- 4  blue
-    "#C040BE",  -- 5  magenta
-    "#00C5C7",  -- 6  cyan
-    "#C7C7C7",  -- 7  white
+    "#14191E",       -- 0  black
+    "#B43C2A",       -- 1  red
+    "#00C200",       -- 2  green
+    "#C7C400",       -- 3  yellow
+    "#2744C7",       -- 4  blue
+    "#C040BE",       -- 5  magenta
+    "#00C5C7",       -- 6  cyan
+    "#C7C7C7",       -- 7  white
   },
 
   brights = {
-    "#686868",  -- 8  bright black
-    "#DD7975",  -- 9  bright red
-    "#58E790",  -- 10 bright green
-    "#ECE100",  -- 11 bright yellow
-    "#A7ABF2",  -- 12 bright blue
-    "#E17EE1",  -- 13 bright magenta
-    "#60FDFF",  -- 14 bright cyan
-    "#FFFFFF",  -- 15 bright white
+    "#686868",     -- 8  bright black
+    "#DD7975",     -- 9  bright red
+    "#58E790",     -- 10 bright green
+    "#ECE100",     -- 11 bright yellow
+    "#A7ABF2",     -- 12 bright blue
+    "#E17EE1",     -- 13 bright magenta
+    "#60FDFF",     -- 14 bright cyan
+    "#FFFFFF",     -- 15 bright white
   },
 }
 
-config.color_scheme = "Custom Dark"
+config.show_new_tab_button_in_tab_bar = false
+config.use_fancy_tab_bar = false
 
 config.initial_cols = 120
 config.initial_rows = 50
@@ -56,5 +58,18 @@ config.window_padding = {
   top = 0,
   bottom = 0,
 }
+
+config.native_macos_fullscreen_mode = true
+
+-- Détection du système : macOS uniquement
+if wezterm.target_triple:find("darwin") then
+  config.keys = {
+    {
+      key = "f",
+      mods = "CMD|CTRL",
+      action = wezterm.action.ToggleFullScreen,
+    },
+  }
+end
 
 return config
