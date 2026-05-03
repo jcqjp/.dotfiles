a715d26abc1c8714b51ee288d3faf1d450e13173
tp222 <tp222@pm.me>
2026-05-03 11:27:00 +0200

    wezterm small ajust

--- .wezterm.lua
+++ .wezterm.lua
@@ -9,66 +9,120 @@ config.window_decorations = "RESIZE"
 config.window_close_confirmation = "NeverPrompt"
 
 config.colors = {
-  -- Éléments principaux
   foreground = "#DCDCDC",
   background = "#14191E",
 
-  -- Curseur
   cursor_bg = "#FFFFFF",
   cursor_fg = "#000000",
   cursor_border = "#FFFFFF",
 
-  -- Sélection
   selection_bg = "#B3D7FF",
   selection_fg = "#000000",
 
-  -- ANSI (16 couleurs standards)
   ansi = {
-    "#14191E",       -- 0  black
-    "#B43C2A",       -- 1  red
-    "#00C200",       -- 2  green
-    "#C7C400",       -- 3  yellow
-    "#2744C7",       -- 4  blue
-    "#C040BE",       -- 5  magenta
-    "#00C5C7",       -- 6  cyan
-    "#C7C7C7",       -- 7  white
+    "#14191E", "#B43C2A", "#00C200", "#C7C400",
+    "#2744C7", "#C040BE", "#00C5C7", "#C7C7C7",
+  },
+  brights = {
+    "#686868", "#DD7975", "#58E790", "#ECE100",
+    "#A7ABF2", "#E17EE1", "#60FDFF", "#FFFFFF",
   },
 
-  brights = {
-    "#686868",     -- 8  bright black
-    "#DD7975",     -- 9  bright red
-    "#58E790",     -- 10 bright green
-    "#ECE100",     -- 11 bright yellow
-    "#A7ABF2",     -- 12 bright blue
-    "#E17EE1",     -- 13 bright magenta
-    "#60FDFF",     -- 14 bright cyan
-    "#FFFFFF",     -- 15 bright white
+  tab_bar = {
+    background = "#14191E",
+    active_tab = { bg_color = "#2744C7", fg_color = "#FFFFFF" },
+    inactive_tab = { bg_color = "#14191E", fg_color = "#686868" },
+    inactive_tab_edge = "#2744C7",
   },
 }
 
 config.show_new_tab_button_in_tab_bar = false
 config.use_fancy_tab_bar = false
+config.tab_max_width = 12
+config.tab_bar_at_bottom = true
+
+-- Icônes pour les programmes courants (police Nerd Font obligatoire)
+local function get_icon(process_name)
+  local name = process_name:lower()
+  if name:find("zsh") or name:find("bash") or name:find("fish") then
+    return "  " -- terminal shell
+  elseif name:find("vim") or name:find("nvim") then
+    return "  " -- vim/neovim
+  elseif name:find("ssh") then
+    return "  " -- ssh
+  elseif name:find("python") then
+    return "  " -- python
+  elseif name:find("node") then
+    return "  " -- node.js
+  elseif name:find("docker") then
+    return "  " -- docker
+  elseif name:find("git") then
+    return "  " -- git
+  elseif name:find("lazygit") then
+    return "  " -- lazygit
+  elseif name:find("htop") or name:find("top") then
+    return "  " -- htop/top
+  elseif name:find("ranger") or name:find("lf") then
+    return "  " -- file manager
+  elseif name:find("tmux") then
+    return "  " -- tmux
+  elseif name:find("rust") then
+    return "  " -- rust
+  elseif name:find("go") then
+    return "  " -- go
+  elseif name:find("ruby") then
+    return "  " -- ruby
+  elseif name:find("lua") then
+    return "  " -- lua
+  elseif name:find("psql") or name:find("postgres") then
+    return "  " -- postgresql
+  elseif name:find("mysql") or name:find("mariadb") then
+    return "  " -- mysql/mariadb
+  elseif name:find("redis") then
+    return "  " -- redis
+  elseif name:find("npm") or name:find("yarn") then
+    return "  " -- npm/yarn
+  elseif name:find("brew") then
+    return "  " -- homebrew
+  else
+    return "  " -- terminal générique
+  end
+end
+
+wezterm.on("format-tab-title", function(tab)
+  local title = tab.active_pane.title or "no title"
+  local index = tab.tab_index + 1
+  local icon = ""
+  local pane = tab.active_pane
+  if pane.foreground_process_name then
+    icon = get_icon(pane.foreground_process_name)
+  end
+
+  local text = " " .. index .. ":" .. icon .. title .. " "
+  if tab.is_active then
+    return {
+      { Background = { Color = "#2744C7" } },
+      { Foreground = { Color = "#FFFFFF" } },
+      { Text = text },
+    }
+  else
+    return {
+      { Background = { Color = "#14191E" } },
+      { Foreground = { Color = "#686868" } },
+      { Text = text },
+    }
+  end
+end)
 
 config.initial_cols = 120
 config.initial_rows = 50
 
-config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
-}
-
+config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
 config.native_macos_fullscreen_mode = true
 
--- Détection du système : macOS uniquement
 if wezterm.target_triple:find("darwin") then
   config.keys = {
-    {
-      key = "f",
-      mods = "CMD|CTRL",
-      action = wezterm.action.ToggleFullScreen,
-    },
+    { key = "f", mods = "CMD|CTRL", action = wezterm.action.ToggleFullScreen },
   }
 end
 
